1aa1a713158284c3269cdd0b05e5ec7013c46aca
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-25 18:54:33 +0600

    added services section and load data

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         <div className="lg:w-1/2 space-y-5 p-4">
           <h3 className="text-3xl text-orange-500 font-bold">About Us</h3>
           <h1 className="text-5xl font-bold">
-            We are qualified & of experience in this field
+            We are qualified <br /> & of experience <br /> in this field
           </h1>
           <p className="py-6 text-gray-600">
             There are many variations of passages of Lorem Ipsum available, but
