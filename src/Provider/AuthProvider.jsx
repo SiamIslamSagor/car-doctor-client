ff3165e06f29c4d4c11da28f18504673e59ebe7f
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-25 22:52:32 +0600

    added login  and register page also setup authProvider to acces value any where

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import PropTypes from "prop-types";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
