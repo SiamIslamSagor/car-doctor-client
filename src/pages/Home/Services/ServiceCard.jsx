8419bc2f30d6adea466fc333f687bacc47c0ab07
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-26 16:30:52 +0600

    loaded data form server side

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
@@ -10,7 +12,12 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title">{title}</h2>
         <p className="text-xl text-orange-500">Price: ${price}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="flex items-center gap-2 text-orange-600 font-bold uppercase ">
+              Book Now{" "}
+              <BsArrowRight className="text-lg duration-700 hover:scale-x-150		"></BsArrowRight>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
